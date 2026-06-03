97bba3ffc7d418315df0a1e2bfec7e92d93b1155
Jakub Šabla <sabla71006@mot.sps-dopravni.cz>
2026-06-03 08:13:49 +0200

    Update projekt_3.py

--- projekt_3.py
+++ projekt_3.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 import csv
 
 def main():
-    # 1. Kontrola zda je zadáno správně odkaz a název souboru jak se stáhne
+    
     if len(sys.argv) != 3:
         print("CHYBA: Musíš zadat odkaz a název souboru.")
         print("Příklad: python projekt_3.py <odkaz> <vysledky.csv>")
@@ -19,10 +19,10 @@ def main():
     url_adresat = sys.argv[1]
     vystupni_soubor = sys.argv[2]
 
-    # Vzor vyžaduje přesně tento text na začátku
+    
     print(f"ZÍSKÁVÁM DATA Z URL: {url_adresat}")
     
-    # 2. Stažení dat ze stránky
+    
     try:
         odpoved = requests.get(url_adresat)
         soup = BeautifulSoup(odpoved.text, 'html.parser')
@@ -37,7 +37,7 @@ def main():
     odkaz_zaklad = "https://volby.cz/pls/ps2017nss/"
     radky = soup.find_all('tr')
     
-    # 3. Procházení obcí
+    
     for radek in radky:
         td_kod = radek.find('td', class_='cislo')
         td_nazev = radek.find('td', class_='overflow_name')
@@ -49,24 +49,24 @@ def main():
             relativni_link = td_kod.find('a')['href']
             odkaz_obec = odkaz_zaklad + relativni_link
             
-            # Výpis každé stahované URL adrese obce
+            
             print(f"ZÍSKÁVÁM DATA Z URL: {odkaz_obec}")
             
             res_obec = requests.get(odkaz_obec)
             soup_obec = BeautifulSoup(res_obec.text, 'html.parser')
 
-            # Načtení základních počtů
+            
             volici = soup_obec.find('td', headers='sa2').text.replace('\xa0', ' ').strip()
             obalky = soup_obec.find('td', headers='sa3').text.replace('\xa0', ' ').strip()
             hlasy = soup_obec.find('td', headers='sa6').text.replace('\xa0', ' ').strip()
 
-            # Načtení názvů stran a procentuálních výsledků 
+            
             strany_nazvy = soup_obec.find_all('td', headers=['t1sa1 t1sb2', 't2sa1 t2sb2'])
             
-            # Vzor na obrázku ukládá procenta, bere sloupce s procenty
+            
             strany_procenta = soup_obec.find_all('td', headers=['t1sa4 t1sb5', 't2sa4 t2sb5'])
             if not strany_procenta:
-                # Záloha tabulek
+                
                 strany_procenta = soup_obec.find_all('td', headers=['t1sa3 t1sb4', 't2sa3 t2sb4'])
 
             if not hlavicka_hotova:
@@ -80,7 +80,7 @@ def main():
             
             obce_data.append(radek_obce)
 
-    # 4. Zápis do CSV
+    # 
     print(f"UKLÁDÁM DATA DO SOUBORU: {vystupni_soubor}")
     with open(vystupni_soubor, mode='w', newline='', encoding='utf-8-sig') as f:
         writer = csv.writer(f)
